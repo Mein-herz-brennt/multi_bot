9a471ad9c117d1aafe704a38882f8ac8f346633d
icaev <icaevvlad81@gmail.com>
2022-07-26 14:52:31 +0300

    update work with user panel

--- db.py
+++ db.py
@@ -106,6 +106,7 @@ class DateBase:
         finally:
             self.conn.close()
 
+    # work with new user
     def add_new_user_info(self, user_id: int, first_name: str, last_name: str, username: str) -> bool:
         try:
             self.cursor.execute("""INSERT INTO users VALUES (?,?,?,?)""", (user_id, first_name, last_name, username))
@@ -115,6 +116,28 @@ class DateBase:
             time = str(datetime.datetime.now())
             logging.warning(msg=time + " -*-add new user-*- " + str(e))
             return False
+        finally:
+            self.conn.close()
+
+    def get_all_users_info(self) -> list:
+        try:
+            return [{"id_": i[0], "first_name": i[1], "last_name": i[2], "username": i[3]}
+                    for i in self.cursor.execute("""SELECT * FROM users""").fetchall()]
+        except Exception as e:
+            time = str(datetime.datetime.now())
+            logging.warning(msg=time + " -*-get all info about all user-*- " + str(e))
+            return []
+        finally:
+            self.conn.close()
+
+    def get_one_user_info(self, user_id: int) -> list:
+        try:
+            return [{"id_": i[0], "first_name": i[1], "last_name": i[2], "username": i[3]}
+                    for i in self.cursor.execute("""SELECT * FROM users WHERE id_ = ?""", (user_id,)).fetchall()]
+        except Exception as e:
+            time = str(datetime.datetime.now())
+            logging.warning(msg=time + " -*-get all info about one user-*- " + str(e))
+            return []
         finally:
             self.conn.close()
 
